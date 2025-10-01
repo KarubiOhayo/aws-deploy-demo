0657bd3dad6b80561d9d6d969d09a869c26b2d2c
김규섭 <pens9053@gmail.com>
2025-10-01 10:23:12 +0900

    init commit

---
+++ src/main/java/com/codeit/aws/config/PropertyConfig.java
@@ -0,0 +1,10 @@
+package com.codeit.aws.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+
+@Configuration
+@PropertySource("")
+public class PropertyConfig {
+
+}
